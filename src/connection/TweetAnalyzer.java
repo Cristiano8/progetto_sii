d3a90bf185186993772358b9f3f79d8d542a159c
FedericoMartinelli <fed.martinelli94@gmail.com>
2017-03-09 18:50:19 +0100

    altre modifiche

--- src/connection/TweetAnalyzer.java
+++ src/connection/TweetAnalyzer.java
@@ -69,7 +69,7 @@ public class TweetAnalyzer {
 		}
 
 		System.out.println(hm.toString());
-		System.out.println(hm.getTopHashtagsByValue(40).toString());
+		System.out.println(hm.getTopHashtags(5).toString());
 
 		return tweets;
 
